12d8938e8049d41f1cbc670d2dfa8e7c704a7000
nageshbhagelli <bhagellinagesh@gmail.com>
2025-04-24 21:31:07 +0530

    files added

---
+++ TopologicalOrdering.java
@@ -0,0 +1,5 @@
+// To compute a topological ordering of G:
+// Find a node v with no incoming edges and order it first
+// Delete v from G
+// Recursively compute a topological ordering of G−{v}
+// and append this order after v
