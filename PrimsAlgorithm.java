111d3648823159e32f58b6a152ec96501288dba2
nageshbhagelli <bhagellinagesh@gmail.com>
2025-06-16 17:53:34 +0530

    files added

---
+++ PrimsAlgorithm.java
@@ -0,0 +1,9 @@
+/*
+ * Suppose say you are working for a company that is building a new office
+ * complex. The company needs to install network cables to connect all the
+ * office buildings in the most cost-effective way. The buildings are located in
+ * a grid, and there are possible routes between pairs of buildings. Each route
+ * has an associated cost based on the distance between buildings. Your task is
+ * to help the company minimize the total cost of wiring the buildings together
+ * using Prim’s Algorithm to find the Minimum Spanning Tree (MST) of the graph.
+ */
