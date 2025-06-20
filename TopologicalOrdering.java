0c7bd09e60b8e4d2ca076e55dbfb398e1f8b30a8
nageshbhagelli <bhagellinagesh@gmail.com>
2025-06-20 22:55:44 +0530

    files added

--- TopologicalOrdering.java
+++ TopologicalOrdering.java
@@ -1,3 +1,12 @@
+/*
+ * Suppose You are developing a task scheduler for a project management tool.
+ * The project consists of several tasks, and each task may depend on other
+ * tasks to be completed before it can start. The tasks and their dependencies
+ * are represented as a directed acyclic graph (DAG). Your task is to write a
+ * program that schedules the tasks in such a way that each task starts only
+ * when all of its dependencies have been completed.
+ */
+
 // To compute a topological ordering of G:
 // Find a node v with no incoming edges and order it first
 // Delete v from G
