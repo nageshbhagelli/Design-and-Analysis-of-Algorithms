1198a95f1bba2decbee7e52f60c7a551ff1e5c9d
nageshbhagelli <bhagellinagesh@gmail.com>
2025-06-20 22:57:21 +0530

    files added

--- TopologicalOrdering.java
+++ TopologicalOrdering.java
@@ -15,7 +15,7 @@
 
 import java.util.*;
 
-public class TaskScheduler {
+public class TopologicalOrdering {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
