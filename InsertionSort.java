8ef0c272a75a2a408e663b128d52675c7ef0ccb1
nageshbhagelli <bhagellinagesh@gmail.com>
2025-07-09 23:08:20 +0530

    QuickSort

--- InsertionSort.java
+++ InsertionSort.java
@@ -4,7 +4,6 @@ public class InsertionSort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 1️⃣ Take input
         System.out.print("Enter number of elements: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
@@ -14,10 +13,8 @@ public class InsertionSort {
             arr[i] = sc.nextInt();
         }
 
-        // 2️⃣ Perform Insertion Sort
         insertionSort(arr);
 
-        // 3️⃣ Print sorted array
         System.out.println("Sorted array:");
         for (int num : arr) {
             System.out.print(num + " ");
@@ -26,18 +23,16 @@ public class InsertionSort {
         sc.close();
     }
 
-    // Insertion Sort function
     static void insertionSort(int[] arr) {
         int n = arr.length;
         for (int i = 1; i < n; i++) {
             int key = arr[i];
             int j = i - 1;
 
-            // Move elements greater than key to one position ahead
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
-            }
+            }s
             arr[j + 1] = key;
         }
     }
