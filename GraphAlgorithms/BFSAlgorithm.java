38fd1c76d2a5553e3d3e34414c9bac37129f9ab7
nageshbhagelli <bhagellinagesh@gmail.com>
2025-07-19 17:36:52 +0530

    BFS

---
+++ GraphAlgorithms/BFSAlgorithm.java
@@ -0,0 +1,13 @@
+// BFS(s):
+// Mark s as "Visited".
+// Initialize R={s}.
+// Define layer L0={s}.
+// While Li is not empty
+// For each node u∈Li
+// Consider each edge (u,v) incident to u
+// If v is not marked "Visited" then
+// Mark v "Visited"
+// Add v to the set R and to layer Li+1
+// Endif
+// Endfor
+// Endwhile
