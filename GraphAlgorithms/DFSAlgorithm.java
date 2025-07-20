4489c31101c4c1faa694d4d7f4ce60345c8c460a
nageshbhagelli <bhagellinagesh@gmail.com>
2025-07-20 21:02:39 +0530

    DFS

---
+++ GraphAlgorithms/DFSAlgorithm.java
@@ -0,0 +1,8 @@
+// DFS(u):
+// Mark u as "Visited" and add u to R.
+// For each edge (u,v) incident to u
+// If v is not marked "Visited" then
+// Add v to R.
+// Recursively invoke DFS(v).
+// Endif
+// Endfor
